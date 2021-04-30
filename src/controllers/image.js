cfc39d28f48ddbc041d2bb32a66856de19ea9e06
João Paulo Chagas da Cruz <joaopauloch17@gmail.com>
2021-04-30 02:48:27 -0300

    AWS Rekognition installed and working great

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,4 +1,5 @@
 const Image = require("../models/Image");
+const aws = require("aws-sdk");
 
 exports.get = async (req, res) => {
   const posts = await Image.find();
@@ -7,6 +8,31 @@ exports.get = async (req, res) => {
 
 exports.post = async (req, res) => {
   const { originalname: name, size, key, location: url = "" } = req.file;
+  const bucket = process.env.BUCKET_NAME;
+  const photo  = key;
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: bucket,
+        Name: photo
+      },
+    },
+    MaxLabels: 10
+  }
+  const client = new aws.Rekognition({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DEFAULT_REGION, 
+  });
+  client.detectLabels(params, function(err, response) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      response.Labels.forEach(label => {
+        console.log(`Label:      ${label.Name}`)
+      })
+    }
+  });
   const imageContain = [];
   const post = await Image.create({
     name,
